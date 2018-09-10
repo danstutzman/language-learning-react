809fc5c68f8ec0181199fbb8fb1899a43aa638be
Daniel Stutzman <dtstutz@gmail.com>
2018-09-10 15:54:46 -0600

    Play syllables from Home screen

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 // @flow
 import cards from './cards.js'
+import {convertBuckwalterToArabic} from './buckwalter/convertBuckwalter.js'
 import Quiz from './Quiz.js'
 import React from 'react'
+import splitIntoSyllables from './buckwalter/splitIntoSyllables.js'
+import WORDS from './buckwalter/WORDS.js'
 
 type Props = {|
   speakText: (script: string) => void,
@@ -27,9 +30,38 @@ export default class Home extends React.Component<Props, State> {
   onCloseQuiz = () =>
     this.setState({ showQuiz: false })
 
+  onClickWord = (e: Event) => {
+    const wordBuckwalter = (e.target: any).getAttribute('data-word')
+    this.props.speakText(convertBuckwalterToArabic(wordBuckwalter))
+  }
+
   render() {
     return <div>
       <h2>Home</h2>
+
+      {WORDS.map((wordBuckwalter: string, i: number) =>
+        <li key={i}>
+          {splitIntoSyllables(wordBuckwalter).map(
+            (syllable: [string | null, string, string | null]) => {
+              const syllableBuckwalter =
+                `${syllable[0] || ''}${syllable[1]}${syllable[2] || ''}`
+                  .replace('ll', 'l')
+              const displayed = syllableBuckwalter
+                .replace('*', 'dh')
+                .replace('E', '`')
+                .replace('x', 'kh')
+                .replace('$', 'sh')
+                .replace('g', 'gh')
+              return <button
+                key={syllableBuckwalter}
+                onClick={this.onClickWord}
+                data-word={syllableBuckwalter + 'o'}>
+                {displayed}
+              </button>
+            })}
+        </li>
+      )}
+
       <button onClick={this.onClickShowQuiz}>Show Quiz</button>
 
       {this.state.showQuiz &&
