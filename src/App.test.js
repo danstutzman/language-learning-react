60a96953c67bd990bfc4278b8e60ac7252baf239
Daniel Stutzman <dtstutz@gmail.com>
2018-09-23 14:20:33 -0600

    Refactor to remove duplication

--- src/App.test.js
+++ src/App.test.js
@@ -53,21 +53,21 @@ it('decides pronunciation for phonemes', () => {
 })
 
 it('adds context to atoms', () => {
-  const add = addContextToAtoms
-  expect(add([
-    { atom:'b', beginPunctuation:'', endPunctuation:'', endsMorpheme: false, endsWord: false },
-    { atom:'a', beginPunctuation:'', endPunctuation:'', endsMorpheme: false, endsWord: false },
-    { atom:'d', beginPunctuation:'', endPunctuation:'', endsMorpheme: false, endsWord: false },
-    { atom:'a', beginPunctuation:'', endPunctuation:'', endsMorpheme: true, endsWord: true },
-  ])).toEqual([
-    { atom:'b', left2: null, left: null, right: 'a', right2: 'd', endsSyllable: false, endsMorpheme: false,
-      endsWord: false, beginPunctuation:'', endPunctuation: '' },
-    { atom:'a', left2: null, left: 'b',  right: 'd', right2: 'a', endsSyllable: true, endsMorpheme: false,
-      endsWord: false, beginPunctuation:'', endPunctuation: '' },
-    { atom:'d', left2: 'b', left: 'a',  right: 'a', right2: null, endsSyllable: false, endsMorpheme: false,
-      endsWord: false, beginPunctuation:'', endPunctuation: '' },
-    { atom:'a', left2: 'a', left: 'd',  right: null, right2: null, endsSyllable: true, endsMorpheme: true,
-      endsWord: true, beginPunctuation:'', endPunctuation: '' },
+  const default1 = {
+    beginPunctuation: '',
+    endPunctuation: '',
+    endsMorpheme: false,
+    endsWord: false,
+  }
+  const bada1 = { ...default1, atom:'b' }
+  const bada2 = { ...default1, atom:'a' }
+  const bada3 = { ...default1, atom:'d' }
+  const bada4 = { ...default1, atom:'a', endsMorpheme: true, endsWord: true }
+  expect(addContextToAtoms([bada1, bada2, bada3, bada4])).toEqual([
+    { ...bada1,left2:null,left:null,right:'a', right2:'d', endsSyllable:false },
+    { ...bada2,left2:null,left:'b', right:'d', right2:'a', endsSyllable:true },
+    { ...bada3,left2:'b', left:'a', right:'a', right2:null,endsSyllable:false },
+    { ...bada4,left2:'a', left:'d', right:null,right2:null,endsSyllable:true },
   ])
 })
 
