9f5c88b783e8dde6d344df12a06fe248ddc13cfd
Dan Stutzman <dan@danstutzman.com>
2018-12-19 22:34:18 -0700

    Rip out groupAtomsIntoSyllables

--- src/buckwalter/groupAtomsIntoSyllables.test.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import groupAtomsIntoSyllables from './groupAtomsIntoSyllables.js'
-
-it('groups atoms into syllables', () => {
-  const group = groupAtomsIntoSyllables
-  expect(group(['b', 'a', 'd'])).toEqual([['b', 'a', 'd']])
-  expect(group(['b', 'a', 'd', 'a'])).toEqual([['b', 'a'], ['d', 'a']])
-})
